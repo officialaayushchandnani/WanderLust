49f5c1e81b35f16ea96148eefa389dcecefb9065
Aayush Chandnani <136909577+officialaayushchandnani@users.noreply.github.com>
2025-02-21 02:34:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const app = express();
 
 // Use the correct MongoDB URL from environment variables
 
-const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = mongodb://127.0.0.1:27017/wanderlust;
 
 // Connect to MongoDB
 async function main() {
